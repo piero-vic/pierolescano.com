3460738bf2e308a2fd1723abc2d1ccb7fc15f991
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-12-08 00:04:52 -0500

    Add modal

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
-// import ProjectModal from './ProjectModal';
+import ProjectModal from './ProjectModal';
 import styles from './ProjectCard.module.css'
 
 function ProjectCard(props) {
   const { project } = props;
 
+  const [clicked, setClicked] = useState(false);
+  const toggleModal = () => setClicked(!clicked)
+
   return (
-    <div className={styles.projectCard}>
+    <>
+    <div className={styles.projectCard} onClick={toggleModal}>
       <h3>{project.name}</h3>
       <p>{project.description}</p>
       <div className={styles.footer}>
@@ -17,8 +21,9 @@ function ProjectCard(props) {
         </ul>
         <a href={project.repoLink} target="_blank">Source Code</a>
       </div>
-      {/* <ProjectModal clicked={clicked}/> */}
     </div>
+    {clicked && <ProjectModal project={project} toggleModal={toggleModal}/>}
+    </>
   )
 }
 
---
+++ src/components/ProjectModal.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './ProjectModal.module.css';
+
+function ProjectModal(props) {
+  const { project, toggleModal } = props
+
+  return (
+    <div className={`${styles.modalBg} ${styles.modalOff}`}>
+      <div className={styles.modal}>
+        <h2>{project.name}</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore voluptates tempore veniam earum atque odit quod laborum expedita aut itaque. Quasi tenetur error necessitatibus commodi tempore. Sit quaerat omnis eaque delectus consequuntur, totam in quod blanditiis voluptas alias illum, maxime sed culpa minima eum error, id unde ad assumenda veritatis?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore voluptates tempore veniam earum atque odit quod laborum expedita aut itaque. Quasi tenetur error necessitatibus commodi tempore. Sit quaerat omnis eaque delectus consequuntur, totam in quod blanditiis voluptas alias illum, maxime sed culpa minima eum error, id unde ad assumenda veritatis?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore voluptates tempore veniam earum atque odit quod laborum expedita aut itaque. Quasi tenetur error necessitatibus commodi tempore. Sit quaerat omnis eaque delectus consequuntur, totam in quod blanditiis voluptas alias illum, maxime sed culpa minima eum error, id unde ad assumenda veritatis?</p>
+        <button onClick={toggleModal}>Close</button>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectModal;
