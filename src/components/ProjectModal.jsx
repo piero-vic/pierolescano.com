2de478a1020744d6ebc9f20f5bdab211c8db9438
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-12-10 04:26:20 -0500

    Add and style terminal

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -6,14 +6,19 @@ function ProjectModal(props) {
 
   return (
     <div className={`${styles.modalBg} ${styles.modalOff}`}>
-      <div className={styles.modal} onClick="">
+      <div className={styles.modal}>
         {
-          project.image &&
-          <>
-            <img className={styles.modalImg} src={project.image} alt="" />
-            <hr />
-          </>
+          project.name === 'todo'
+          ? <div id="termynal" data-termynal className={`${styles.terminal} ${styles.shadow}`}>
+              <span data-ty="input">td</span>
+              <br />
+              <span data-ty data-ty-delay="250"> 1 | <span style={{color: "#f7768e"}}>✕</span> Send Report</span>
+              <span data-ty data-ty-delay="300" > 2 | <span style={{color: "#9ece6a"}}>✓</span> Pickup groceries</span>
+              <span data-ty data-ty-delay="350"> 3 | <span style={{color: "#f7768e"}}>✕</span> Update portfolio</span>
+            </div>
+          : project.image && <img className={`${styles.modalImg} ${styles.shadow}`} src={project.image} alt="" />
         }
+        <hr />
         <div className={styles.modalText}>
           <h2>{project.name}</h2>
           <sub className={styles.modalSub}>{project.description}</sub>
