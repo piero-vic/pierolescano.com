15b7d05ad512610d293b8f82e247068edd4d0e91
Piero Lescano <lescaspv@gmail.com>
2021-12-20 19:19:55 -0500

    Add terminal to todo project (#4)
    * Setup termynal
    
    * Add and style terminal
    
    * Replace termynal for a simple image

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -1,28 +1,33 @@
-import React from 'react';
-import styles from './ProjectModal.module.css';
+import React from "react";
+import styles from "./ProjectModal.module.css";
 
 function ProjectModal(props) {
-  const { project, toggleModal } = props
+  const { project, toggleModal } = props;
 
   return (
     <div className={`${styles.modalBg} ${styles.modalOff}`}>
-      <div className={styles.modal} onClick="">
-        {
-          project.image &&
+      <div className={styles.modal}>
+        {project.image && (
           <>
-            <img className={styles.modalImg} src={project.image} alt="" />
+            <img
+              className={`${styles.modalImg} ${styles.shadow}`}
+              src={project.image}
+              alt=""
+            />
             <hr />
           </>
-        }
+        )}
         <div className={styles.modalText}>
           <h2>{project.name}</h2>
           <sub className={styles.modalSub}>{project.description}</sub>
           <p>{project.descriptionLong}</p>
-          <button className={styles.modalBtn} onClick={toggleModal}>Close</button>
+          <button className={styles.modalBtn} onClick={toggleModal}>
+            Close
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default ProjectModal;
