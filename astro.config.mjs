17806e5f6a9873d9bc5f44953483c4e89625f77e
Piero Lescano <pierolescano@protonmail.com>
2022-04-02 15:00:00 -0500

    build: Use Astro integration for TailwindCSS (#39)
    * build: Use Astro integration for TailwindCSS
    
    * chore: Remove unused files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   buildOptions: {
     site: "https://www.pierolescano.com/",
     sitemap: true,
     pageUrlFormat: "directory",
   },
-  integrations: [react()],
+  integrations: [react(), tailwind()],
   vite: {
     ssr: {
       external: ["svgo"],
