04753b4ea14855c7c1a873acc24dec67646f1f66
Piero Lescano <pierolescano@protonmail.com>
2022-04-28 13:46:21 -0500

    build: Add React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.pierolescano.com/",
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   vite: {
     ssr: {
       external: ["svgo"],
