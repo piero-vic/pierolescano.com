c752d9755c5bdfc4a5590cbd71a10f2eb87be325
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-07-25 10:52:56 -0500

    chore: Update projects (#101)
    * refactor: Remove repeated wrappers on different pages
    
    * feat(projects): Add project technologies
    
    * chore: Update projects and links

--- src/data.js
+++ src/data.js
@@ -2,6 +2,7 @@ export const projects = [
   {
     name: "Coin Watcher",
     description: "Web application for consulting crypto prices and news.",
+    technologies: ["React", "TailwindCSS"],
     category: "web",
     image: "/images/coin-watcher.png",
     repoLink: "https://github.com/piero-vic/coin-watcher",
@@ -10,6 +11,7 @@ export const projects = [
   {
     name: "Space Traveler's Hub",
     description: "Single-page application that displays SpaceX rockets and missions.",
+    technologies: ["React", "Redux"],
     category: "web",
     image: "/images/space-hub.png",
     repoLink: "https://github.com/Amrhub/space-travelers-hub",
@@ -22,46 +24,34 @@ export const projects = [
   {
     name: "Coding Bootcamp Webpage",
     description: "Capstone project for Module 1 of Microverse.",
+    technologies: ["HTML", "CSS", "JavaScript"],
     category: "web",
     image: "/images/coding-bootcamp.png",
     repoLink: "https://github.com/piero-vic/coding-bootcamp-website",
     demoLink: "https://piero-vic.github.io/coding-bootcamp-website/",
   },
   {
-    name: "Leaderboard",
-    description: "Leaderboard project for Microverse.",
-    category: "web",
-    image: "/images/leaderboard.png",
-    repoLink: "https://github.com/piero-vic/Leaderboard",
+    name: "cmp-ledger",
+    description: "A nvim-cmp source for ledger accounts.",
+    technologies: ["Lua"],
+    category: "cli",
+    image: null,
+    repoLink: "https://github.com/piero-vic/cmp-ledger",
     demoLink: null,
-  },
-  {
-    name: "ToDo List",
-    description: "A minimal to-do list web application.",
-    category: "web",
-    image: "/images/todo-list.png",
-    repoLink: "https://github.com/piero-vic/to-do-list",
-    demoLink: "https://piero-vic.github.io/to-do-list/dist/",
   },
   {
     name: "ptd",
     description: "A minimal to-do list application for the command line.",
+    technologies: ["Python", "Typer"],
     category: "cli",
     image: null,
     repoLink: "https://github.com/piero-vic/ptd",
     demoLink: null,
   },
   {
-    name: "crypto-prices",
-    description: "Crypto prices for the Ledger CLI.",
-    category: "cli",
-    image: null,
-    repoLink: "https://github.com/piero-vic/crypto-prices",
-    demoLink: null,
-  },
-  {
     name: "ani-py",
     description: "A cli tool to download and stream anime from jkanime.",
+    technologies: ["Python", "Typer"],
     category: "cli",
     image: null,
     repoLink: "https://github.com/piero-vic/ani-py",
@@ -69,10 +59,22 @@ export const projects = [
   },
 ];
 
+export const colors = {
+  CSS: "text-cyan",
+  HTML: "text-red",
+  JavaScript: "text-yellow",
+  Lua: "text-magenta",
+  Python: "text-green",
+  React: "text-blue",
+  Redux: "text-magenta",
+  TailwindCSS: "text-blue",
+  Typer: "text-white",
+};
+
 export const socialMedia = {
   Github: "https://github.com/piero-vic",
   LinkedIn: "https://www.linkedin.com/in/piero-lescano",
-  Twitter: "https://www.twitter.com/v1ccenzo",
+  Twitter: "https://www.twitter.com/piero_codes",
 };
 
 export const twitter = {
