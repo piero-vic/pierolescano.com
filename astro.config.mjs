a3e142c422b47e31b832244d2426bb85aabba265
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-12-16 20:48:44 -0500

    refactor: Add Astro integration for Alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
+import alpine from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.pierolescano.com/",
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [tailwind(), react(), mdx(), alpine()],
   vite: {
     ssr: {
       external: ["svgo"],
