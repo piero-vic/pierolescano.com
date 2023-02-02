11c9d0ccc5bef1ca62716f07c7fc5deffacc4dab
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2023-02-01 19:37:49 -0500

    feat: Migrate to Content Collections

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { z, defineCollection } from "astro:content";
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string().optional(),
+    imageAlt: z.string().optional(),
+    pubDate: z.date(),
+    tags: z.array(z.string()),
+    comments: z.boolean()
+  })
+});
+
+export const collections = {
+  blog: blogCollection,
+};
