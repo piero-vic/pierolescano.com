2213c76541366140fcbd5c83db009ccca6ed4019
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2023-06-28 09:09:03 -0500

    build: Update dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-import alpine from '@astrojs/alpinejs';
+import alpine from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
       black: "#32344a",
