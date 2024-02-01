ebb3ef7e4463c382167b7eb20e434ae8e9ac39bc
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2024-02-01 01:05:59 -0500

    feat: Include full post in RSS feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,9 @@
 import type { APIContext } from "astro";
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function GET(context: APIContext) {
   const posts = await getCollection("blog");
@@ -16,6 +19,7 @@ export async function GET(context: APIContext) {
       description: post.data.description,
       pubDate: post.data.pubDate,
       link: `/blog/${post.slug}`,
+      content: sanitizeHtml(parser.render(post.body)),
     })),
   });
 }
