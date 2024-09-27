577255f66a05dd21c0026334bb819c2435934f0c
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2024-09-26 23:43:54 -0500

    feat: Change date format in posts and remove dayjs

---
+++ src/lib/datetimes.ts
@@ -0,0 +1,11 @@
+export const SHORT_MONTH_DATE_FORMATTER = new Intl.DateTimeFormat("en-US", {
+  year: "numeric",
+  month: "short",
+  day: "numeric",
+});
+
+export const LONG_MONTH_DATE_FORMATTER = new Intl.DateTimeFormat("en-US", {
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+});
