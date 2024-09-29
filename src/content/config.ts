b0bbab39062fade8c93a3b5c3f31a76b8962143e
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2024-09-28 19:23:18 -0500

    feat(notes): Books

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,52 +1,52 @@
 import { z, defineCollection } from "astro:content";
 import { file } from "astro/loaders";
 
-const blogCollection = defineCollection({
-  schema: ({ image }) =>
-    z
-      .object({
-        title: z.string(),
-        description: z.string(),
-        image: image().optional(),
-        imageAlt: z.string().optional(),
-        pubDate: z.date(),
-      })
-      .refine(
-        schema => {
+export const collections = {
+  blog: defineCollection({
+    schema: ({ image }) =>
+      z
+        .object({
+          title: z.string(),
+          description: z.string(),
+          image: image().optional(),
+          imageAlt: z.string().optional(),
+          pubDate: z.date(),
+        })
+        .refine(schema => {
           if (schema.image !== undefined && schema.imageAlt === undefined) {
             return false;
           }
           return true;
-        },
-        { message: "your message" },
-      ),
-});
-
-const projectsCollection = defineCollection({
-  loader: file("src/data/projects.json"),
-  schema: ({ image }) =>
-    z.object({
-      id: z.string(),
-      name: z.string(),
-      description: z.string(),
-      primaryLanguage: z.object({
+        }),
+  }),
+  projects: defineCollection({
+    loader: file("src/data/projects.json"),
+    schema: ({ image }) =>
+      z.object({
+        id: z.string(),
         name: z.string(),
-        color: z.string(),
-      }),
-      image: image().optional(),
-      repoLink: z.string().optional(),
-      demoLink: z.string().optional(),
-      collaborator: z
-        .object({
+        description: z.string(),
+        primaryLanguage: z.object({
           name: z.string(),
-          github: z.string(),
-        })
-        .optional(),
-      favorite: z.boolean(),
+          color: z.string(),
+        }),
+        image: image().optional(),
+        repoLink: z.string().optional(),
+        demoLink: z.string().optional(),
+        collaborator: z
+          .object({
+            name: z.string(),
+            github: z.string(),
+          })
+          .optional(),
+        favorite: z.boolean(),
+      }),
+  }),
+  notes: defineCollection({
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      tags: z.array(z.string()).optional(),
     }),
-});
-
-export const collections = {
-  blog: blogCollection,
-  projects: projectsCollection,
+  }),
 };
---
+++ src/content/notes/books.md
@@ -0,0 +1,27 @@
+---
+title: Books
+description: What I'm reading and would like to read.
+tags: []
+---
+
+## Currently Reading
+
+- [Red Rising](https://www.goodreads.com/book/show/15839976-red-rising) by Pierce Brown
+- [Understanding Distributed Systems](https://understandingdistributed.systems/) by Roberto Vitillo
+
+## Reading Next
+
+Maybe I'll read something from [Standard Ebooks](https://standardebooks.org/).
+
+- Shop Class as Soulcraft by Matthew B. Crawford
+- The Pragmatic Programmer by Andrew Hunt and David Thomas
+- Code: The Hidden Language of Computer Hardware and Software by Charles Petzold
+- How Not to Be Wrong: The Power of Mathematical Thinking by Jordan Ellenberg
+
+## Finished Reading
+
+| Title                                                                                   | Author            | Read       |
+| --------------------------------------------------------------------------------------- | ----------------- | ---------- |
+| [Four Thousand Weeks](https://www.goodreads.com/book/show/54785515-four-thousand-weeks) | Oliver Burkeman   | 2024-09-28 |
+| [Rhythm of War](https://www.goodreads.com/book/show/49021976-rhythm-of-war)             | Brandon Sanderson | 2024-08-25 |
+| [The Road](https://www.goodreads.com/book/show/6288.The_Road)                           | Cormac McCarthy   | 2024-01-05 |
